4bb7d3c95ce10debe8a31e57b56a0ec928ea765e
Intuitive Harmony <j.horst77@gmail.com>
2023-03-27 12:28:48 -0600

    Render data in cat template

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
+# Temporary Data
+cats = [
+  {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
+  {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
+]
+
 # Create your views here:
 # Define the home view
 def home(request):
@@ -8,3 +14,8 @@ def home(request):
 def about(request):
   # Include an .html file extension - unlike when rendering EJS templates
   return render(request, 'about.html')
+def cats_index(request):
+  # Include an .html file extension - unlike when rendering EJS templates
+  return render(request, 'cats/index.html', {
+    'cats': cats
+  })
