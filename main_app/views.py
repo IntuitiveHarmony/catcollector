8b62f2ff3b3019db7cd8df00aa6ac1044006cc96
Intuitive Harmony <j.horst77@gmail.com>
2023-03-28 09:04:29 -0600

    Cat model

--- main_app/views.py
+++ main_app/views.py
@@ -8,15 +8,11 @@ cats = [
 ]
 
 # Create your views here:
-# Define the home view
 def home(request):
-  # Include an .html file extension - unlike when rendering EJS templates
   return render(request, 'home.html')
 def about(request):
-  # Include an .html file extension - unlike when rendering EJS templates
   return render(request, 'about.html')
 def cats_index(request):
-  # Include an .html file extension - unlike when rendering EJS templates
   return render(request, 'cats/index.html', {
     'cats': cats
   })
